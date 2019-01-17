53843c1a77a2a2afd360d737fde8e87489cce316
xiaohp <xiaohp1993@gmail.com>
2019-01-17 15:46:04 +0800

    add types

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
-var express = require('express')
-var graphqlHTTP = require('express-graphql')
-var { buildSchema } = require('graphql')
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-// 使用 GraphQL Schema Language 创建一个 schema
+// 使用 GraphQL schema language 构建一个 schema
 var schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
-`)
+`);
 
-// root 提供所有 API 入口端点相应的解析器函数
+// root 将会提供每个 API 入口端点的解析函数
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
-}
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+};
 
-var app = express()
-
+var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}))
-
-app.listen(4000)
-console.log('Running a GraphQL API server at localhost:4000/graphql')
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
