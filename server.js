0a10acec7d237c1bf577f2a08ef45268c353b3f4
xiaohp <xiaohp1993@gmail.com>
2019-01-17 16:25:50 +0800

    add object types

--- server.js
+++ server.js
@@ -4,33 +4,61 @@ var { buildSchema } = require('graphql');
 
 // 使用 GraphQL schema language 构建一个 schema
 var schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-  }
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
+    type Query {
+        quoteOfTheDay: String
+        random: Float!
+        rollThreeDice: [Int]
+        rollDice(numDice: Int!, numSides: Int): [Int]
+        getDie(numSides: Int): RandomDie
+    }
 `);
+
+// 该类继承 RandomDie GraphQL 类型
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 
 
 // root 将会提供每个 API 入口端点的解析函数
 var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
-  rollDice: function ({numDice, numSides}) {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    getDie: function({numSides}) {
+        return new RandomDie(numSides || 6);
+    },
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    },
+    rollDice: function({numDice, numSides}) {
+        var output = [];
+        for (var i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
     }
-    return output;
-  }
 };
 
 
