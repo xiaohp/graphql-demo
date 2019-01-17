2dd9d0dc32d7c5c4eb88acbca9bbb0d46d1155b4
xiaohp <xiaohp1993@gmail.com>
2019-01-17 15:22:20 +0800

    user express server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var { graphql, buildSchema } = require('graphql')
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
 
+// 使用 GraphQL Schema Language 创建一个 schema
 var schema = buildSchema(`
   type Query {
     hello: String
   }
 `)
 
-var root = { hello: () => 'Hello world!' }
+// root 提供所有 API 入口端点相应的解析器函数
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+}
 
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response)
-})
+var app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
