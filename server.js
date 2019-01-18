b9c502fae9aa8ee719fac29ae6e3f8a6d459192d
xiaohp <xiaohp1993@gmail.com>
2019-01-18 10:48:26 +0800

    format code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // 使用 GraphQL schema language 构建一个 schema
-var schema = buildSchema(`
+const schema = buildSchema(`
     input MessageInput {
       content: String
       author: String
@@ -47,8 +47,8 @@ class RandomDie {
   }
 
   roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
+    let output = [];
+    for (let i = 0; i < numRolls; i++) {
       output.push(this.rollOnce());
     }
     return output;
@@ -65,10 +65,10 @@ class Message {
 }
 
 // 映射 username 到 content
-var fakeDatabase = {};
+const fakeDatabase = {};
 
 // root 将会提供每个 API 入口端点的解析函数
-var root = {
+const root = {
     getDie: function({numSides}) {
         return new RandomDie(numSides || 6);
     },
@@ -82,8 +82,8 @@ var root = {
         return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
     },
     rollDice: function({numDice, numSides}) {
-        var output = [];
-        for (var i = 0; i < numDice; i++) {
+        let output = [];
+        for (let i = 0; i < numDice; i++) {
             output.push(1 + Math.floor(Math.random() * (numSides || 6)));
         }
         return output;
@@ -96,7 +96,7 @@ var root = {
     },
     createMessage: function ({input}) {
       // Create a random id for our "database".
-      var id = require('crypto').randomBytes(10).toString('hex');
+      const id = require('crypto').randomBytes(10).toString('hex');
 
       fakeDatabase[id] = input;
       return new Message(id, input);
@@ -111,11 +111,12 @@ var root = {
     },
 };
 
-var app = express();
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
